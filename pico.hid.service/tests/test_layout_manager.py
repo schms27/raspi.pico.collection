670b30fc3d40bb423c70faadd11bcdfd22b17bdd
Simu <schms27@gmail.com>
2021-10-24 02:30:40 +0200

    more testing

--- pico.hid.service/tests/test_layout_manager.py
+++ pico.hid.service/tests/test_layout_manager.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import patch
+from macro_enums import Order
 from settings import Settings
 
 from layout_manager import LayoutManager
@@ -15,6 +16,13 @@ class TestLayoutManager(unittest.TestCase):
         self.assertEqual(self.layout_manager.currentLayoutIndex, 0)
         self.layout_manager.swapLayout("FORWARD")
         self.assertEqual(self.layout_manager.currentLayoutIndex, 1)
+        self.layout_manager.swapLayout("BACKWARD")
+        self.assertEqual(self.layout_manager.currentLayoutIndex, 0)
+
+    def test_get_action(self):
+        action = self.layout_manager.getAction(Order.SHORT_PRESSED, 1)
+        self.assertIsNotNone(action)
+        self.assertEqual(action['type'] == 'SOUND_MIXER')
        
 if __name__ == '__main__':
     unittest.main()
