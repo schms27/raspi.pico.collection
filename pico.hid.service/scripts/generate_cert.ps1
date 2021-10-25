73711a8c981bcf76701c0f1cc29c023c18cddc70
Simu <schms27@gmail.com>
2021-10-25 21:04:41 +0200

    added some useful commands to scripts

--- pico.hid.service/scripts/generate_cert.ps1
+++ pico.hid.service/scripts/generate_cert.ps1
@@ -1,2 +1,7 @@
 
 New-SelfSignedCertificate -CertStoreLocation cert:\currentuser\my -Subject "CN=Test Code Signing" -KeyAlgorithm RSA -KeyLength 2048 -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider" -KeyExportPolicy Exportable -KeyUsage DigitalSignature -Type CodeSigningCert
+
+certutil -encode '.\cert.pfx' 'cert_base64.txt'
+certutil -decode 'cert_base64.txt' '.\cert_new.pfx'
+
+certutil -f -p "supersafepassword" -importpfx .\cert_new.pfx
